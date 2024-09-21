1971a81ae96010f48dd70e025aafe8e9df7ed95a
leonellcruz1107 <l.cruz@prostrive.io>
2024-09-21 14:02:10 +0800

    add registry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import TravelGuide from "@/components/home/TravelGuide";
 import Faq from "@/components/home/Faq";
 import Rsvp from "@/components/home/Rsvp";
 import { motion } from "framer-motion";
+import Registry from "@/components/home/Registry";
 export default function Home() {
   return (
     <motion.div
@@ -22,6 +23,7 @@ export default function Home() {
       <WeddingParty />
       <TravelGuide />
       <Faq />
+      <Registry />
       <Rsvp />
     </motion.div>
   );
---
+++ src/components/home/Registry.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+
+const Registry = () => {
+  return (
+    <div className="space-y-6 py-12">
+      <p className="text-main text-[40px] md:text-[60px] text-center cursive font-bold">
+        Registry
+      </p>
+      <div className="max-w-[840px] mx-auto">
+        <p className="text-center">
+          Your presence at our wedding is the greatest gift we could ask for! If
+          you feel inclined to give us a wedding gift, a contribution would help
+          get our newlywed life off to a great start.
+        </p>
+      </div>
+
+      <div className="flex justify-center max-w-[500px] mx-auto">
+        <Image
+          width={1000}
+          height={1000}
+          alt="qr"
+          src="/images/qr.jpg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Registry;
